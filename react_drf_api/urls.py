fbf8ae71afcada98e78b5e1df86cceb3fb3031e3
Lamin Saidy <nimalydias@gmail.com>
2024-10-22 12:04:26 +0000

    Create Profile Details View: Get & Put

--- react_drf_api/urls.py
+++ react_drf_api/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', admin.site.urls),
-    path('profiles/', include('profiles.urls')),
+    path('admin', admin.site.urls),
+    path('', include('profiles.urls')),
 
 ]
