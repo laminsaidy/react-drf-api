84f35127306734c54fb845cd3a0eb9d2e5d400e8
Lamin Saidy <nimalydias@gmail.com>
2024-10-23 12:38:45 +0000

    Create Likes Generic Views and Follower Generic Views

--- likes/views.py
+++ likes/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, permissions
-from drf_api.permissions import IsOwnerOrReadOnly
+from react_drf_api.permissions import IsOwnerOrReadOnly
 from likes.models import Like
 from likes.serializers import LikeSerializer
 
