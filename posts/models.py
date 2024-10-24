759b4ff8a7bf0da44dbe96b991f6122ac13d7921
Lamin Saidy <nimalydias@gmail.com>
2024-10-24 11:04:28 +0000

    Create Post and Profile filters to API

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,8 @@
-from django.db import models
 
-# Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
 
+# Create your models here.
 
 class Post(models.Model):
     """
