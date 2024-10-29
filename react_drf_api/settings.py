e8a9f6d8de0b6fb71d890743dd466c3bc13fd247
Lamin Saidy <nimalydias@gmail.com>
2024-10-29 12:03:56 +0000

    Changing the CLIENT_ORIGIN for more security

--- react_drf_api/settings.py
+++ react_drf_api/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import re
 
 from pathlib import Path
 import os
@@ -67,7 +68,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'my-react-drf-api.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'my-react-drf-api.herokuapp.com',
+os.environ.get('ALLOWED_HOST')
+]
 
 
 # Application definition
@@ -112,9 +115,10 @@ MIDDLEWARE = [
 
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
