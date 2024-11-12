1972bc3e8105bb9b914b642ea72c586b693b023a
Lamin Saidy <nimalydias@gmail.com>
2024-11-12 18:47:24 +0000

    Update the urls.py file

--- react_drf_api/urls.py
+++ react_drf_api/urls.py
@@ -3,17 +3,17 @@ from django.urls import path, include
 from .views import root_route, logout_route, custom_login, YourDataView
 
 urlpatterns = [
-    path('', root_route),  # Root route
-    path('login/', custom_login),  # Custom login
-    path('logout/', logout_route),  # Custom logout
-    path('api/auth/', include('dj_rest_auth.urls')),  # Default auth routes
+    path('', root_route),  
+    path('login/', custom_login),  
+    path('logout/', logout_route),  
+    path('api/auth/', include('dj_rest_auth.urls')),  
+    path('dj-rest-auth/', include('dj_rest_auth.urls')), 
     path('yourdata/', YourDataView.as_view(), name='yourdata'),
-    path('admin/', admin.site.urls),  # Admin
-    path('api-auth/', include('rest_framework.urls')),  # DRF auth
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),  # dj-rest-auth
-    path('', include('profiles.urls')),  # Include profiles URLs
-    path('', include('posts.urls')),  # Include posts URLs
-    path('', include('comments.urls')),  # Include comments URLs
-    path('', include('likes.urls')),  # Include likes URLs
-    path('', include('followers.urls')),  # Include followers URLs
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),  
+    path('profiles/', include('profiles.urls')),  
+    path('posts/', include('posts.urls')),  
+    path('comments/', include('comments.urls')),  
+    path('likes/', include('likes.urls')),  
+    path('followers/', include('followers.urls')),  
 ]
